e6b50b1f76804b62d3338af6e1fff72f563256f8
SergioCerveraJ <seceji@gmail.com>
2023-11-21 12:59:56 +0100

    21/11/23

---
+++ src/CosasConLaNueva/LazarElHilo.java
@@ -0,0 +1,24 @@
+package CosasConLaNueva;
+
+public class LazarElHilo {
+    public static void main(String[] args) {
+
+
+        Thread h1 = new Thread(new hiloEjemplo3("H1"));
+        Thread h2 = new Thread(new hiloEjemplo3("H2"));
+
+        h1.start();
+        h2.start();
+
+
+        try {
+            h1.join();
+            h2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Hilo principal interrumpido");
+        }
+        System.out.println("Hilo principal terminado");
+
+
+    }
+}
---
+++ src/CosasConLaNueva/hiloEjemplo3.java
@@ -0,0 +1,31 @@
+package CosasConLaNueva;
+
+import java.util.Random;
+
+public class hiloEjemplo3 implements Runnable {
+
+    private final String nombre;
+
+    hiloEjemplo3(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    @Override
+    public void run() {
+
+        System.out.printf("Hola, soy el hilo: %s.\n", this.nombre);
+        Random r = new Random();
+        for (int i = 0; i < 5; i++) {
+            int pausa = 10 + r.nextInt(500 - 10);
+            System.out.printf("Hilo: %s hace pausa de %d ms.\n",
+                    this.nombre, pausa);
+            try {
+                Thread.sleep(pausa);
+            } catch (InterruptedException e) {
+                System.out.printf("Hilo %s interruptido.\n", this.nombre);
+            }
+        }
+        System.out.printf("Hilo %s terminado.\n", this.nombre);
+    }
+}
